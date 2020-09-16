2dea17a3cefe8c0339544b887a90ee7cdebb954d
PleekMan <agusanzorena@gmail.com>
2020-09-16 23:05:14 +0200

    All pages and thumbs added

--- js/fuzzy_zines.js
+++ js/fuzzy_zines.js
@@ -2,7 +2,7 @@
 
 $(init);
 
-var zines = ['membrane_phoebe', 'membrane_phoebe2'];
+var zines = ['membrane', 'human-bites-dog','doodle','crumbling1','crumbling2','border-crossing-kit'];
 
 function init() {
 
@@ -16,8 +16,8 @@ function run() {
 
   if (fb.length != 0) {
     fb.turn({
-      width: 800,
-      height: 600,
+      width: 842,
+      height: 595,
       autoCenter: true
     });
 
@@ -45,39 +45,40 @@ function initFlipBook() {
 
   // console.log("SelectedFlipBook: " + selectedFlipBook);
 
-  let path = 'zines/' + zines[selectedFlipBook] + '/';
+  let path = 'zines/' + zines[0] + '/';
 
   $('#flipbookContainer')
     .append($('<div id="flipbook"/>')
-      .append($('<div/>', { 'class': 'hard' }).css({ 'background-image': 'url(' + path + 'FRONT.png)' }))
-      .append($('<div/>', { 'class': 'hard' }).css({ 'background-image': 'url(' + path + 'INNERFRONT.png)' }))
-      .append($('<div/>', { 'class': 'pg1' }).css({ 'background-image': 'url(' + path + '1.png)' }))
-      .append($('<div/>', { 'class': 'pg2' }).css({ 'background-image': 'url(' + path + '2.png)' }))
-      .append($('<div/>', { 'class': 'pg3' }).css({ 'background-image': 'url(' + path + '3.png)' }))
-      .append($('<div/>', { 'class': 'pg4' }).css({ 'background-image': 'url(' + path + '4.png)' }))
-      .append($('<div/>', { 'class': 'pg5' }).css({ 'background-image': 'url(' + path + '5.png)' }))
+      .append($('<div/>', { 'class': 'hard' }).css({ 'background-image': 'url(' + path + '1.jpg)' }))
+      .append($('<div/>', { 'class': 'hard' }).css({ 'background-image': 'url(' + path + '2.jpg)' }))
+      .append($('<div/>', { 'class': 'pg1' }).css({ 'background-image': 'url(' + path + '3.jpg)' }))
+      .append($('<div/>', { 'class': 'pg2' }).css({ 'background-image': 'url(' + path + '4.jpg)' }))
+      .append($('<div/>', { 'class': 'pg3' }).css({ 'background-image': 'url(' + path + '5.jpg)' }))
+      .append($('<div/>', { 'class': 'pg4' }).css({ 'background-image': 'url(' + path + '6.jpg)' }))
+      .append($('<div/>', { 'class': 'pg5' }).css({ 'background-image': 'url(' + path + '7.jpg)' }))
+      .append($('<div/>', { 'class': 'pg6' }).css({ 'background-image': 'url(' + path + '8.jpg)' }))
+
 
     );
 
 }
 
-function updateFlipBook() {
+function updateFlipBook(selected) {
 
   // console.log("SelectedFlipBook: " + selectedFlipBook);
 
-  let path = 'zines/' + zines[selectedFlipBook] + '/';
+  let path = 'zines/' + zines[selected] + '/';
 
 
       
-    $("#flipbookContainer").find('[page="1"] > div > div').css('background-image', 'url(' + path + 'FRONT.png)');
-    // .append($('<div/>', { 'class': 'hard' }).css({ 'background-image': 'url(' + path + 'FRONT.png)' }))
-
-      // .append($('<div/>', { 'class': 'hard' }).css({ 'background-image': 'url(' + path + 'INNERFRONT.png)' }))
-      // .append($('<div/>', { 'class': 'pg1' }).css({ 'background-image': 'url(' + path + '1.png)' }))
-      // .append($('<div/>', { 'class': 'pg2' }).css({ 'background-image': 'url(' + path + '2.png)' }))
-      // .append($('<div/>', { 'class': 'pg3' }).css({ 'background-image': 'url(' + path + '3.png)' }))
-      // .append($('<div/>', { 'class': 'pg4' }).css({ 'background-image': 'url(' + path + '4.png)' }))
-      // .append($('<div/>', { 'class': 'pg5' }).css({ 'background-image': 'url(' + path + '5.png)' }))
+    $("#flipbookContainer").find('[page="1"] > div > div').css('background-image', 'url(' + path + '1.jpg)');
+    $("#flipbookContainer").find('[page="2"] > div > div').css('background-image', 'url(' + path + '2.jpg)');
+    $("#flipbookContainer").find('[page="3"] > div > div').css('background-image', 'url(' + path + '3.jpg)');
+    $("#flipbookContainer").find('[page="4"] > div > div').css('background-image', 'url(' + path + '4.jpg)');
+    $("#flipbookContainer").find('[page="5"] > div > div').css('background-image', 'url(' + path + '5.jpg)');
+    $("#flipbookContainer").find('[page="6"] > div > div').css('background-image', 'url(' + path + '6.jpg)');
+    $("#flipbookContainer").find('[page="7"] > div > div').css('background-image', 'url(' + path + '7.jpg)');
+    $("#flipbookContainer").find('[page="8"] > div > div').css('background-image', 'url(' + path + '8.jpg)');
 
 
 }
